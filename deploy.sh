f30349a441b04f0ee1918b8750f2566a8c45ea80
Fabian Otto <fabian.otto@familienservice.de>
2014-12-19 22:32:55 +0100

    set open read rights for uploaded files

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ SERVER=hosteurope
 R_PATH=/webroot/henrich/lesehuette.de/public_html/
 PORT=22
 USER=root
-OPTIONS=$@
+OPTIONS="$@ --chmod=Fo+r"
 KEY="$HOME/.ssh/hosteurope.rsa.pub"
 jekyll build && rsync -avz --rsh="ssh -p$PORT -l$USER -i $KEY" $OPTIONS _site/ $SERVER:$R_PATH
