3362b3f8ae182a37ad27bb106a43eb0a88712146
Fabian Otto <fabian.otto@familienservice.de>
2017-12-17 23:50:08 +0100

    deleting _site from repot

--- _site/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SERVER=hosteurope
-R_PATH=/webroot/henrich/lesehuette.de/public_html/
-PORT=22
-USER=root
-OPTIONS="$@ --chmod=Fo+r"
-KEY="$HOME/.ssh/hosteurope.rsa.pub"
-jekyll build && rsync -avz --rsh="ssh -p$PORT -l$USER -i $KEY" $OPTIONS _site/ $SERVER:$R_PATH
